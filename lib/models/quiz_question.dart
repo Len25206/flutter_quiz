d1e094503316b7e228d4843e385913a7c9c933e1
Len25206 <84762185+Len25206@users.noreply.github.com>
2025-03-17 23:27:29 +0800

    fix

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -2,4 +2,10 @@ class QuizQuestion {
   const QuizQuestion(this.text, this.answer);
   final String text;
   final List<String> answer;
+
+  List<String> getShuffleAnswer() {
+    final shuffleList = List.of(answer);
+    shuffleList.shuffle();
+    return shuffleList;
+  }
 }
